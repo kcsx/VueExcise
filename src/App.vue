3106ef261af61fb1a48601d1e5c132449c061a61
kaiyu <1865400033@.e.gzhu.edu.cn>
2021-04-26 15:51:40 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>{{ title }}</h1>
   </div>
 </template>
 
+<script>
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+export default {
+  data: () => ({
+    title: "Sssss",
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
